5552e5c5d02f79388cd2ba323fcd73dd974b27dc
HiroFive <vanohahj5555@gmail.com>
2021-04-30 20:24:21 +0300

    Temp file delete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './styles/app/App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Auth from './pages/auth/Auth';
-import Temp from './pages/home/Temp';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='*'component={Temp} />
         <Route path='/home' component={Home}/>
         <Route path="/auth" component={Auth}/>
       </Switch>
