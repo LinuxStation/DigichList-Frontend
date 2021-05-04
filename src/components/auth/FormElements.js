b27b041922fcb3f5d0303126b8e94c9735122527
HiroFive <vanohahj5555@gmail.com>
2021-05-04 19:53:55 +0300

    Styles change

--- src/components/auth/FormElements.js
+++ src/components/auth/FormElements.js
@@ -8,140 +8,14 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import FormStyle from './FormStyle';
 
 import PropTypes from 'prop-types';
 
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        minWidth: 275,
-        maxWidth: 350,
-        marginTop: theme.spacing(15),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.05)',
-        '& .MuiSvgIcon-root':{
-            fontSize: 28,
-        },
-        '& .MuiFormLabel-root': {
-            '&.Mui-focused': {
-                color: '#0d6efd',
-            }
-        },
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: 'rgb(206, 212, 218)',
-                borderWidth: 1.5,
-            },
-            '&:hover fieldset': {
-                borderColor: 'rgb(134, 183, 254)',
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: 'rgb(134, 183, 254)',
-            },
-        },
-    },
-    cardContent: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: '#0d6efd',
-        width: theme.spacing(5.5),
-        height: theme.spacing(5.5),
-    },
-    checkBox: {
-        marginLeft: 4,
-        '&:hover': {
-            backgroundColor: 'transparent',
-        },
-    },
-    checkBoxLabel: {
-        marginBottom: 6,
-    },
-    icon: {
-        border: '1px solid rgba(0,0,0,.25)',
-        borderRadius: 3.5,
-        width: 14,
-        height: 14,
-        backgroundColor: '#f5f8fa',
-        '$root.Mui-focusVisible &': {
-            outline: '2px auto rgba(19,124,189,.6)',
-            outlineOffset: 2,
-        },
-        'input:hover ~ &': {
-            backgroundColor: '#ebf1f5',
-        },
-        'input:disabled ~ &': {
-            boxShadow: 'none',
-            background: 'rgba(206,217,224,.5)',
-        },
-    },
-    checkedIcon: {
-        borderRadius: 3.5,
-        backgroundColor: '#0d6efd',
-        '&:before': {
-            display: 'block',
-            width: 16,
-            height: 16,
-            backgroundImage:
-                "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
-                " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
-                "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
-            content: '""',
-        },
-        'input:hover ~ &': {
-            backgroundColor: '#005EEA',
-        },
-    },
-    formInput: {
-        borderRadius: 3.5,
-        borderColor: '#e1e5eb',
-        '& label': {
-            fontSize: 14,
-        },
-        '& .MuiInputBase-root fieldset legend': {
-            '& span': {
-                fontSize: 10,
-            },
-        },
-    },
-    submitBtn: {
-        color: 'white',
-        textTransform: 'none',
-        backgroundColor: '#0d6efd',
-        '&:hover': {
-            backgroundColor: '#005EEA',
-        }
-    },
-    resetPassword: {
-        marginTop: 8,
-        textAlign: 'center',
-        fontSize: 14,
-        '& a': {
-            color: '#0d6efd',
-            textDecoration: 'none',
-            '&:hover': {
-                textDecoration: 'underline',
-            }
-        }
-    },
-    helpText: {
-        color: 'rgb(108, 117, 125)',
-        fontSize: 12,
-        margin: '-2px 2px 8px 2px',
-    }
-}));
-
 export function StyledCheckbox(props) {
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <FormControlLabel
             className={styles.checkBoxLabel}
@@ -163,7 +37,7 @@ export function SubmitBtn(props) {
     SubmitBtn.propTypes = {
         text: PropTypes.string,
     }
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <Button
             className={styles.submitBtn}
@@ -180,7 +54,7 @@ export function HelpingText(props) {
         text: PropTypes.string,
         component: PropTypes.object,
     }
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <Typography variant="body2" className={styles.helpText}>
            {props.text}  {props.component}
@@ -192,7 +66,7 @@ export function ResetLink(props) {
         text: PropTypes.string,
         to: PropTypes.string,
     }
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <Typography className={styles.resetPassword}>
             <Link to={`/auth${props.to}`}>
@@ -211,18 +85,17 @@ export function FormInput(props) {
         maxLength: PropTypes.number,
         autoComplete: PropTypes.string,
         onChange: PropTypes.func,
+        onBlur: PropTypes.func,
     }
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <TextField
-            inputProps={{
-                maxLength: props.maxLength,
-            }}
             className={styles.formInput}
             variant="outlined"
             margin="normal"
             fullWidth
             size="small"
+            onBlur = {props.onBlur}
             onChange = {props.onChange}
             value={props.value}
             label={props.label}
@@ -237,7 +110,7 @@ export function FormCard(props){
         title: PropTypes.string,
         icon: PropTypes.object,
     }
-    const styles = useStyles();
+    const styles = FormStyle();
     return (
         <Container component='main' maxWidth='xs'>
             <Card className={styles.root}>
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
-import { FormInput, StyledCheckbox, SubmitBtn, ResetLink, FormCard } from '../../components/auth/FormElements'
+import {  StyledCheckbox, SubmitBtn, ResetLink, FormCard } from '../../components/auth/FormElements';
+import { Formik } from "formik";
+import { CssBaseline} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import FormStyle from '../../components/auth/FormStyle'
+import * as Yup from "yup";
 
-export default function auth() {
-    return (
-        <FormCard
-            icon={<LockOpenOutlinedIcon/>}
-            title='Login in Admin panel'
-            components={
-                <form>
-                    <FormInput type="email" label="Email Address" autoComplete='email' />
-                    <FormInput type="password" label="Password" autoComplete='password' />
-                    <StyledCheckbox />
-                    <SubmitBtn text='Login' />
-                    <ResetLink text='Forgot Password?' to='/forgot-password' />
-                </form>
-            }
-        />
-    )
-}
+const loginForm = () => (
+    <Formik
+        initialValues={{
+            email: '',
+        }}
+        validationSchema={Yup.object().shape({
+            email: Yup.string()
+                .email('Invalid email'),
+        })}
+        onSubmit={values => {
+            console.log(values);
+        }}
+        >
+        {function (formik){
+            const styles = FormStyle()
+            return (
+            <FormCard
+                icon={<LockOpenOutlinedIcon />}
+                title='Login in Admin panel'
+                components={
+                    <form onSubmit={formik.handleSubmit}>
+                        <CssBaseline />
+                        <TextField
+                            error={formik.errors.email=='Invalid email'}
+                            className={styles.formInput}
+                            variant="outlined"
+                            margin="normal"
+                            helperText={formik.errors.email}
+                            {...formik.getFieldProps('email')}
+                            fullWidth
+                            size="small"
+                            label="Email Address"
+                            type="email"
+                            id="email"
+                        />
+                         <TextField
+                            className={styles.formInput}
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            size="small"
+                            label="Password"
+                            type="password"
+                            id="password"
+                        />
+                        <StyledCheckbox />
+                        <SubmitBtn text='Login' />
+                        <ResetLink text='Forgot Password?' to='/forgot-password' />
+                    </form>
+                }
+            />)
+        }}
+    </Formik>
+);
+
+
+export default loginForm;
