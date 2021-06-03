2dc2abb9c1ca2a7212d02effd386357292193129
HiroFive <vanohahj5555@gmail.com>
2021-06-03 21:04:46 +0300

    created basic API request

--- src/components/workflow/SideMenu.js
+++ src/components/workflow/SideMenu.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
+import React from "react";
+import PropTypes from 'prop-types';
 
 import clsx from "clsx";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -12,7 +14,8 @@ import IconButton from "@material-ui/core/IconButton";
 import Container from "@material-ui/core/Container";
 
 import WorkflowStyle from "./WorkflowStyle";
-import PropTypes from 'prop-types';
+import { withStyles } from "@material-ui/core/styles";
+
 
 import ListMenuItems from "./ListMenuItems";
 
@@ -22,54 +25,65 @@ import MenuIcon from "@material-ui/icons/Menu";
 import PowerSettingsNewOutlinedIcon from '@material-ui/icons/PowerSettingsNewOutlined';
 
 
-export default function SideMenu(props) {
-    SideMenu.propTypes = {
-        body: PropTypes.object,
+class SideMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false,
+            body: props.body
+        }
+        this.handleCloseDraw = this.handleCloseDraw.bind(this)
+        this.handleDrawer = this.handleDrawer.bind(this)
     }
-    
-    const classes = WorkflowStyle();
-    const [open, setOpen] = useState(false);
 
-    const handleCloseDraw  = () => {
-        setOpen(false);
+    handleCloseDraw() {
+        this.setState({ open: false })
     }
-    const handleDrawer = () => {
-        (!open) ? setOpen(true) : setOpen(false);
-    };
+    handleDrawer() {
+        (!this.state.open) ? this.setState({ open: true }) : this.setState({ open: false })
+    }
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                }}
-                open={open}
-            >
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawer}>
-                        {open ? <ChevronLeftIcon className={classes.menuIcon} /> : <MenuIcon className={classes.menuIcon} />}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List className={classes.list} >
-                    <ListMenuItems handleCloseDraw={handleCloseDraw}/>
-                </List>
-                <List className={`${classes.list} ${classes.logoutList}`}>
-                    <ListItem button>
-                        <ListItemIcon className={classes.menuIcon}>
-                            <PowerSettingsNewOutlinedIcon />
-                        </ListItemIcon>
-                        <ListItemText className={classes.listText} primary="Log Out" />
-                    </ListItem>
-                </List>
-            </Drawer>
-            <main className={classes.content}>
-                <Container maxWidth="xl" className={classes.container}>
-                    {props.body}
-                </Container>
-            </main>
-        </div>
-    );
+    render() {
+        const { classes } = this.props
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: clsx(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+                    }}
+                    open={this.state.open}
+                >
+                    <div className={classes.toolbarIcon}>
+                        <IconButton onClick={this.handleDrawer}>
+                            {this.state.open ? <ChevronLeftIcon className={classes.menuIcon} /> : <MenuIcon className={classes.menuIcon} />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List className={classes.list} >
+                        <ListMenuItems handleCloseDraw={this.handleCloseDraw} />
+                    </List>
+                    <List className={`${classes.list} ${classes.logoutList}`}>
+                        <ListItem button>
+                            <ListItemIcon className={classes.menuIcon}>
+                                <PowerSettingsNewOutlinedIcon />
+                            </ListItemIcon>
+                            <ListItemText className={classes.listText} primary="Log Out" />
+                        </ListItem>
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <Container maxWidth="xl" className={classes.container}>
+                        {this.state.body}
+                    </Container>
+                </main>
+            </div>
+        );
+    }
 }
+SideMenu.propTypes = {
+    body: PropTypes.object,
+}
+
+export default withStyles(WorkflowStyle, { withTheme: true })(SideMenu)
